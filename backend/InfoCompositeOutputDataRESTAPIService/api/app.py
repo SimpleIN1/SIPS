59d236a03a1ad9b955bb05804fd639e324279c0a
SimpleIN1 <zimforshint_akross@mail.ru>
2026-06-06 18:09:36 +0700

    correct swagger scheme

--- backend/InfoCompositeOutputDataRESTAPIService/api/app.py
+++ backend/InfoCompositeOutputDataRESTAPIService/api/app.py
@@ -43,7 +43,7 @@ api = Api(
     description="Сервис предоставления информации о выходных данных "
                 "после обработки вычислительным комплексом для оперативной "
                 "обработки данных радиометра VIIRS спутников Suomi-NPP, NOAA-20",
-    # schemes=["http", "https"],
+    schemes=["http", "https"],
     terms="https://www.google.com/policies/terms/"
 )
 
