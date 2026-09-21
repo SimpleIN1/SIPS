0376b680ecac4ce359a3dee6172277db6c185fae
Григорий <zimforshint_akross@mail.ru>
2026-09-21 11:00:12 +0700

    changed AuthenticationService

--- backend/AuthenticationService/AccountProject/AuthApp/serializers.py
+++ backend/AuthenticationService/AccountProject/AuthApp/serializers.py
@@ -12,6 +12,7 @@ class TokenAppendField:
     def get_token(cls, user):
         token = super().get_token(user)
         token['email'] = user.email
+        # token['groups'] = str(list(user.groups.values_list("name", flat=True)))
         return token
 
 
